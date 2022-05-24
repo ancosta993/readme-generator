05b52f798f3bdd1495987cd891e625a11352e802
Anthony Dcosta <adcosta2000@gmail.com>
2022-05-23 23:16:01 -0400

    initializing the folders

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquire');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
